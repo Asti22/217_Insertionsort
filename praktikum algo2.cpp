290a5e821db291e14766f517712a350276e43b5d
Asti22 <uun3461@gmail.com>
2024-03-14 14:34:21 +0700

    menambahkan variable temp

--- praktikum algo2.cpp
+++ praktikum algo2.cpp
@@ -3,6 +3,16 @@
 #include <iostream>
 using namespace std;
 
+void insertionsSort() {
+	int i, j, n, temp;
+	int arr[20];
+
+	for (i = 1; i <= n - 1; i++) { // step 1
+		
+		 
+	}
+}
+
 int main()
 {
    
